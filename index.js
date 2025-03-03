3857abec8d5362cd7380556a112c2fa813a0daf2
rajeshkamboj <rajeshkamboj33@gmail.com>
2025-03-03 12:56:39 +0530

    I have updated it according to Harry Bhaiya's tutorials as Replit no longer supporting me

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-let frand = "Romu";
-console.log(frand.substring(0,2)); // Output: "Ro"
-console.log(frand.substring(0,1)); // Output: "R"
-console.log(frand.substring(0,3)); // Output: "Rom"
-console.log(frand.substring(3));    // Output: "u"
-
-let newFrand = frand.substring(0, 2) + 's' + frand.substring(3);
-console.log(newFrand); // Output: "Rosu"
-
-// let frand = "Romu";
-// let frandArray = frand.split('');
-// frandArray[2] = 's';
-// let newFrand = frandArray.join('');
-// console.log(newFrand); // Output: "Rosu"
+console.log("Hello World")
