6ce80eb3c174619621c2cf1ac09927a9acad9372
rajeshkamboj <rajeshkamboj33@gmail.com>
2025-03-13 19:27:35 +0530

    Learnt While Loop

---
+++ Chapter03/10_while_loop.js
@@ -0,0 +1,48 @@
+// Question: Write a program to print the multiplication table of a given number using while loop.
+
+let n = 5;
+
+let i=1;
+while(i<=10){
+    console.log(n + " * " + i + " = " + n*i);
+    i++;
+}
+
+// Output:
+// Enter a number: 5
+// 5 * 1 = 5
+// 5 * 2 = 10
+// 5 * 3 = 15
+// 5 * 4 = 20
+// 5 * 5 = 25
+// 5 * 6 = 30
+// 5 * 7 = 35
+// 5 * 8 = 40
+// 5 * 9 = 45
+// 5 * 10 = 50
+
+// q.2 write a very simple program to demonstrate while loop
+let j = 1;
+while(j<=5){
+    console.log("Hello World");
+    j++;
+}
+
+// Output:
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+
+
+// Q.3 Write a program to demonstrate a simple do while loop program
+let k = 1;
+do{
+    console.log("Hello World");
+    k++;
+}
+while(k<=50);    
+
+// Output:
+// Hello World 
